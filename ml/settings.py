bc046cb9e441856bc3e1246b39ab3194dede2dcd
achilles050 <thorn3579@gmail.com>
2020-12-07 11:57:39 +0700

    2nd commit

--- ml/settings.py
+++ ml/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -121,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
